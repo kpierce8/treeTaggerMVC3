2c03c3f28d1637faed4d97a5a2565126f0be9d28
Ken Pierce <kpierce8@gmail.com>
2011-07-10 10:07:41 -0700

    service works locally not on web

---
+++ treeTaggerMVC3/treeObservationService.svc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Data.Objects;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+using treeTaggerMVC3.Models;
+using System.Data.Entity.Infrastructure;
+
+namespace treeTaggerMVC3
+{
+    public class treeObservationService : DataService< treeTaggerMVC3Entities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            config.SetEntitySetAccessRule("treeObservations", EntitySetRights.AllRead);
+           // config.SetServiceOperationAccessRule("treeObservationService", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+        }
+
+        //protected override ObjectContext CreateDataSource()
+        //{
+        //    var ctx = ((IObjectContextAdapter)new treeOBSEntities()).ObjectContext;
+        //    ctx.ContextOptions.ProxyCreationEnabled = false;
+        //    return ctx;
+        //}
+    }
+}
